a2b2a31ebf596d456dfa1655b6c5c50eb90dfd36
Arkady Rosenberg <intelligent002@gmail.com>
2025-04-14 20:57:19 +0300

    ignores

---
+++ swarm-build.sh
@@ -0,0 +1,4 @@
+docker build -t rest ./rest
+docker build -t worker ./worker
+docker build -t grafana ./grafana
+docker build -t prometheus ./prometheus
--- swarm-init.sh
+++ swarm-init.sh
@@ -1,1 +1,3 @@
 docker swarm init
+
+docker network create --driver overlay --attachable sync-to-async
