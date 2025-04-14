6a418a3fee9988ade27af07bdbd54704c6ee8d4d
Arkady Rosenberg <intelligent002@gmail.com>
2025-04-14 20:58:20 +0300

    ignores

--- network-install.sh
+++
@@ -1,6 +0,0 @@
-docker network create \
-  --driver overlay \
-  --attachable \
-  --subnet 172.30.1.0/24 \
-  --gateway 172.30.1.1 \
-  sync-to-async
--- swarm-init.sh
+++ swarm-init.sh
@@ -1,3 +1,8 @@
 docker swarm init
 
-docker network create --driver overlay --attachable sync-to-async
+docker network create \
+  --driver overlay \
+  --attachable \
+  --subnet 172.30.1.0/24 \
+  --gateway 172.30.1.1 \
+  sync-to-async
