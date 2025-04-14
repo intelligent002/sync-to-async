f4bba14a97bdb5e91dceeadc821e58cc64d66765
Arkady Rosenberg <intelligent002@gmail.com>
2025-04-14 19:34:57 +0300

    initial

--- buil-n-run.sh
+++
@@ -1,8 +0,0 @@
-docker build -t rest ./rest
-docker build -t worker ./worker
-docker build -t prometheus ./prometheus
-docker build -t grafana ./grafana
-
-docker swarm init
-docker stack rm sync-to-async
-docker stack deploy -c .\docker-swarm.yml sync-to-async
